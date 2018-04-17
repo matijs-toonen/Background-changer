bf82713a243907e631b626392312ad06cdab0ff2
Matijs Toonen <matijstoonen@gmail.com>
2018-04-17 19:03:08 +0200

    imported background webscraper
    imported existing webscraper with background changer

---
+++ Webscraper/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Webscraper.Classes;
+
+namespace Webscraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = @"http://1920x1080hdwallpapers.com/anime/";
+            Worker.ImageSetter(url);
+            Environment.Exit(0);
+        }
+    }
+}
