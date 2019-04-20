ff6b6fc7ba2c4a93a1ae9e3db68cc4ae0ea6edfd
Matijs <matijstoonen@gmail.com>
2019-04-20 22:04:21 +0100

    error handeling

--- ImageSetter/Workflow/WallpaperChanger.cs
+++ ImageSetter/Workflow/WallpaperChanger.cs
@@ -20,7 +20,6 @@ namespace ImageSetter.Workflow
         const UInt32 SPIF_UPDATEINIFILE = 0x01;
         const UInt32 SPIF_SENDWININICHANGE = 0x02;
 
-        private static List<string> ImageList = new List<string>();
         private static Random rnd = new Random();
 
         private static RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
@@ -47,8 +46,15 @@ namespace ImageSetter.Workflow
             if (page != 0)
                 url += $@"page/{page}";
 
-            var document = Scraper.GetHtmlDocument(url);
-            DownloadImage(document);
+            try
+            {
+                var document = Scraper.GetHtmlDocument(url);
+                DownloadImage(document);
+            }
+            catch (Exception)
+            {
+                NoConnExistingImage();
+            }
         }
 
         private static void BlackListCurrentBackground()
@@ -79,54 +85,60 @@ namespace ImageSetter.Workflow
 
         private static void DownloadImage(HtmlDocument document)
         {
+            var imageList = new List<string>();
             foreach (var result in document.DocumentNode.Descendants("img").Select(x => x.Attributes["src"]))
             {
                 if (result.Value.Contains("wallpaper"))
                 {
                     var res = result.Value.LastIndexOf("-");
                     var full = result.Value.Remove(res, 8);
-                    ImageList.Add(full);
+                    imageList.Add(full);
                 }
             }
 
-            RemoveBlackListed();
+            imageList = RemoveBlackListed(imageList);
 
-            if (!ImageList.Any())
+            if (!imageList.Any())
                 return;
 
-            int pictureNum = rnd.Next(0, ImageList.Count - 1);
-            var diff1 = ImageList[pictureNum].LastIndexOf(@"/");
+            int pictureNum = rnd.Next(0, imageList.Count - 1);
+            var diff1 = imageList[pictureNum].LastIndexOf(@"/");
             diff1++;
-            var diff2 = ImageList[pictureNum].LastIndexOf("g");
+            var diff2 = imageList[pictureNum].LastIndexOf("g");
             diff2++;
             var diff = diff2 - diff1;
 
-            var file = ImageList[pictureNum].Substring(diff1, diff);
+            var file = imageList[pictureNum].Substring(diff1, diff);
             var fullName = $@"E:\webscraper\{file}";
 
             if (!File.Exists(fullName))
             {
-                var image = Scraper.Webclient.DownloadData(ImageList[pictureNum]);
+                var image = Scraper.Webclient.DownloadData(imageList[pictureNum]);
                 File.WriteAllBytes(fullName, image);
             }
 
             SetImage(fullName);
         }
 
-        private static void RemoveBlackListed()
+        private static List<string> RemoveBlackListed(List<string> images)
         {
             var path = new FileInfo(@"E:\webscraper\Config\blacklist.txt");
             if (path.Exists)
             {
                 var blackListedItems = File.ReadAllLines(path.FullName);
-                ImageList.RemoveAll(image => IsBlackListed(image, blackListedItems));   
+                images.RemoveAll(image => IsBlackListed(image, blackListedItems));   
             }
+
+            return images;
         }
 
         private static bool IsBlackListed(string picture, string[] blackListedItems)
         { 
             foreach (string item in blackListedItems)
             {
+                if (string.IsNullOrWhiteSpace(item))
+                    continue;
+
                 if (picture.Contains(item))
                     return true;
             }
@@ -136,8 +148,9 @@ namespace ImageSetter.Workflow
 
         private static void NoConnExistingImage()
         {
-            var files = Directory.GetFiles(@"E:\webscraper\", "*.*");
-            var file = files[rnd.Next(files.Length)];
+            var files = Directory.GetFiles(@"E:\webscraper\", "*.*").ToList();
+            files = RemoveBlackListed(files);
+            var file = files[rnd.Next(files.Count) - 1];
 
             SetImage(file);
         }
